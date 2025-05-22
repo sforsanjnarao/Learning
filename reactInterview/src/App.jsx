243638ccb717b56b5addc44667ceef45fd5b62c2
sforsanjnarao <sanjnarao26@gmail.com>
2025-05-22 17:23:27 +0530

    Add Counter component with increment, decrement, and reset functionality

--- reactInterview/src/App.jsx
+++ reactInterview/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { PostComponent } from './Post'
+import Counter from './Counter'
 
 function App() {
   return (
     <div>
-      
+      <Counter/>
     </div>
   )
 }
---
+++ reactInterview/src/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+function Counter() {
+  return (
+    <div>
+        <Counting/>
+    </div>
+  )
+}
+
+export default Counter
+
+
+function Counting(){
+    const [count, setCount]=useState(0)
+    return(
+        <div>
+            <div>
+                <h1>{count}</h1>
+                <button onClick={()=>setCount(count +1)} >increse</button>
+            </div>
+            <div>
+                <button onClick={()=>setCount(count-1)}>decrese</button>
+            </div>
+            <div>
+                <button onClick={()=>setCount(0)}>reset</button>
+            </div>
+        </div>
+
+    )
+}
