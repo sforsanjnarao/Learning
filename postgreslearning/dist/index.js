453c8b51e2e424bd6c42bd6d511e52a1bfc7d3f4
sforsanjnarao <sanjnarao26@gmail.com>
2025-07-03 10:01:32 +0530

    Add Express server setup and user signup endpoint; update package.json dependencies

--- postgreslearning/dist/index.js
+++ postgreslearning/dist/index.js
@@ -8,9 +8,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
+const pg_1 = require("pg"); //it's like mongoose(ODM) to which helps talk to the database
 const pgClient = new pg_1.Client('postgresql://neondb_owner:npg_dGukJq30yrRQ@ep-royal-sea-a1seykjz-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+//another way of building connection
 // const pgClient2= new Client({
 //     user:"neondb_owner",
 //     password:"npg_dGukJq30yrRQ",
@@ -21,9 +28,16 @@ const pgClient = new pg_1.Client('postgresql://neondb_owner:npg_dGukJq30yrRQ@ep-
 // })
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield pgClient.connect();
-        const response = yield pgClient.query("SELECT * FROM users;");
-        console.log(response.rows);
+        yield pgClient.connect(); //like mongoose.connect()
     });
 }
 main();
+app.post('/signup', (res, req) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, email, password } = req.body;
+    //we can use the validation
+    const User = yield pgClient.query(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`);
+    res.status(200).json(console.log('YOU are going write'));
+}));
+app.listen(3000, () => {
+    console.log('server is on the run');
+});
--- postgreslearning/src/index.ts
+++ postgreslearning/src/index.ts
@@ -1,8 +1,11 @@
 import { Client } from "pg"; //it's like mongoose(ODM) to which helps talk to the database
 const pgClient= new Client('postgresql://neondb_owner:npg_dGukJq30yrRQ@ep-royal-sea-a1seykjz-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 //buildinging connection
+import { Response, Request } from "express";
+import express from 'express'
+const app= express()
 
-
+app.use(express.json())
 
 //another way of building connection
 
@@ -17,8 +20,20 @@ const pgClient= new Client('postgresql://neondb_owner:npg_dGukJq30yrRQ@ep-royal-
 // })
 
 async function main() {
-    await pgClient.connect()
-    const response=await pgClient.query("SELECT * FROM users;")
-    console.log(response.rows)
+    await pgClient.connect() //like mongoose.connect()
 }
 main()
+
+app.post('/signup', async (res: Response, req: Request)=>{
+    const {username, email, password}=req.body
+    //we can use the validation
+    const User=await pgClient.query(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`)
+    res.status(200).json(
+        console.log('YOU are going write')
+    )
+
+})
+
+app.listen(3000,()=>{
+    console.log('server is on the run')
+})
