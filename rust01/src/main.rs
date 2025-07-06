5cc43911a430398d498c07b769a434fb97740902
sforsanjnarao <sanjnarao26@gmail.com>
2025-07-06 12:44:05 +0530

    Enhance main function with character extraction and sum calculation; add mutable variable demonstration

--- rust01/src/main.rs
+++ rust01/src/main.rs
@@ -25,9 +25,26 @@ fn main() {
 
     let char1=greeting.chars().nth(5);
     match char1{
-        Some(c )=>println!("{}", c),
+        Some(c )=>println!("char1: {}", c),
         None=>println!("no character in this index")
     }
 
+    let char2=greeting.chars().nth(1);
+    println!("char2: {}", char2.unwrap());
 
+    // for i in 0..10{
+    //     print!(" i:{}", i)
+    // }
+    let mut m=10;
+    m=20;                   //as we know all varables in rust is unmutable in rust and we can make it mutable with use of "mut" 
+    println!("{}",m);
+
+    let total_sum=sum(x,y);
+    println!("Sum of {} and {} is {}",x,y,total_sum)
+
+
+}
+
+fn  sum(x:i8,y: i8)->i8 {
+    return x+y
 }
