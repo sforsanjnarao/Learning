38d7b964a57c625a3247ecc04861c5434ed1a5a8
sforsanjnarao <sanjnarao26@gmail.com>
2025-08-01 23:51:19 +0530

    Refactor App component by commenting out unused state and effects

--- reactInterview/src/App.jsx
+++ reactInterview/src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+import React from 'react'
+
 import { PostComponent } from './Post'
 import Counter from './Counter'
 import Interval from './Interval'
@@ -6,13 +8,13 @@ import { BrowserRouter } from 'react-router-dom'
 import Routeing from './components/Routeing'
 
 function App() {
-  let [countVisible,setCountVisible]=useState(true)
-  useEffect(()=>{
-  setInterval(()=>{
-    setCountVisible(countVisible=>!countVisible)
-  },5000)
+  // let [countVisible,setCountVisible]=useState(true)
+  // useEffect(()=>{
+  // setInterval(()=>{
+  //   setCountVisible(countVisible=>!countVisible)
+  // },5000)
 
-  },[])
+  // },[])
   return (
     // <div>
     //   {countVisible ?<Interval/>: <h1>Nothing is here</h1>}
